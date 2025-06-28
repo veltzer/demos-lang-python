59aaf3254ecec0524c7c25eb24e87848b3d8be09
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-28 23:09:58 +0300

    no commit message given

--- src/examples/short/modules/numpy/linspace.py
+++ src/examples/short/modules/numpy/linspace.py
@@ -3,16 +3,16 @@
 import numpy as np
 
 # Create a 2x3 array of all zeros
-my_array = np.zeros((2,3))
-print(my_array)
+my_array1 = np.zeros((2,3))
+print(my_array1)
 
 # [[ 0.  0.  0.]
 #  [ 0.  0.  0.]]
 
 # Create an array from a list
 my_list = [1, 2, 3]
-my_array = np.array(my_list)
-print(my_array)
+my_array2= np.array(my_list)
+print(my_array2)
 
 # [1 2 3]
 
