1eaa86a440b3b0b78381d764d78c30eac0cbf0f7
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-08 04:07:13 +0300

    linting

--- src/examples/short/language/context_managers/no_exceptions.py
+++ src/examples/short/language/context_managers/no_exceptions.py
@@ -1,7 +1,11 @@
+"""
+This context manager suppresses all exceptions
+"""
+
 class SuppressAll:
     def __enter__(self):
         return self
-    
+
     def __exit__(self, exc_type, exc_value, traceback):
         # Return True to suppress any exception
         return True
@@ -10,6 +14,7 @@ class SuppressAll:
 with SuppressAll():
     print("This will run")
     raise ValueError("This error will be suppressed")
+    # pylint: disable=unreachable
     print("This won't run because of the exception above")
 
 print("This will run - exception was suppressed")
