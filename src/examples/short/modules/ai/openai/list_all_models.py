5aebf2f32506ca2c93e0cbbe2babb31be3a43f6d
Mark Veltzer <mark.veltzer@gmail.com>
2025-10-27 10:15:42 +0200

    no commit message given

--- src/examples/short/modules/ai/openai/list_all_models.py
+++ src/examples/short/modules/ai/openai/list_all_models.py
@@ -7,7 +7,9 @@ import passpy
 import openai
 
 store = passpy.Store()
-api_key = store.get_key("keys/openai").rstrip()
+api_key = store.get_key("keys/openai")
+assert api_key is not None
+api_key = api_key.rstrip()
 client = openai.OpenAI(api_key=api_key)
 models = client.models.list()
 for model in models.data:
--- src/examples/short/modules/ai/openai/simple.py
+++ src/examples/short/modules/ai/openai/simple.py
@@ -6,8 +6,9 @@ import passpy
 import openai
 
 store = passpy.Store()
-api_key = store.get_key("keys/openai").rstrip()
-# print(f"api_key is [{api_key}]")
+api_key = store.get_key("keys/openai")
+assert api_key is not None
+api_key = api_key.rstrip()
 
 client = openai.OpenAI(api_key=api_key)
 models = client.models.list()
