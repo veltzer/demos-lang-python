2c839471aa97412be04e4604fc4fab6475aed930
Mark Veltzer <mark.veltzer@gmail.com>
2026-07-30 11:57:20 +0300

    no commit message given

--- src/examples/short/modules/whisper/basic.py
+++ src/examples/short/modules/whisper/basic.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python
+
 """
 Simple example of whisper
 
 The module you need to install to make this work is `openai-whisper`
 """
 
+import torch
 import whisper
 # model=whisper.load_model("medium")
 model=whisper.load_model("small")
-result=model.transcribe("data_samples/wav/speaking_en.wav", language="en")
+result=model.transcribe("data_samples/wav/speaking_en.wav", language="en", fp16=torch.cuda.is_available())
 print(result["text"])
