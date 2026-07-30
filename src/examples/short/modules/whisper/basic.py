5cccdca24894e992dbc6f04609d9986661ee396e
Mark Veltzer <mark.veltzer@gmail.com>
2026-07-30 11:18:17 +0300

    no commit message given

--- src/examples/short/modules/whisper/basic.py
+++ src/examples/short/modules/whisper/basic.py
@@ -5,7 +5,7 @@ The module you need to install to make this work is `openai-whisper`
 """
 
 import whisper
-model=whisper.load_model("medium")
-# model=whisper.load_model("small")
-result=model.transcribe("out.wav", language="he")
+# model=whisper.load_model("medium")
+model=whisper.load_model("small")
+result=model.transcribe("out.wav", language="en")
 print(result["text"])
