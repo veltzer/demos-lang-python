e895d3f690330f68866b859d6ea4ca715ff29460
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-15 13:09:40 +0300

    no commit message given

--- src/examples/short/modules/whisper/basic.py
+++ src/examples/short/modules/whisper/basic.py
@@ -6,9 +6,12 @@ Simple example of whisper
 The module you need to install to make this work is `openai-whisper`
 """
 
+import json
 import torch
 import whisper
 # model=whisper.load_model("medium")
 model=whisper.load_model("small")
-result=model.transcribe("data_samples/wav/speaking_en.wav", language="en", fp16=torch.cuda.is_available())
-print(result["text"])
+# result=model.transcribe("data_samples/wav/speaking_en.wav", language="en", fp16=torch.cuda.is_available())
+result=model.transcribe("data_samples/wav/conversation_with_hezi_he.wav", language="he", fp16=torch.cuda.is_available())
+# print(result["text"])
+print(json.dumps(result, indent=4, ensure_ascii=False))
