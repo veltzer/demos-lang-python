27cee8de2068d50e67228ef87d15cafffbc73811
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-21 13:24:30 +0300

    convert config to lua

--- config/project.lua
+++ config/project.lua
@@ -1,8 +1,8 @@
-""" project definitions """
-
-name = "demos-python"
-description_short = "Demos for the Python programming language"
-description_long = """This project explores the python programming language.
+-- project definitions
+NAME = "demos-python"
+DESCRIPTION_SHORT = "Demos for the Python programming language"
+DESCRIPTION_LONG = [[
+This project explores the python programming language.
 The idea is that when you are programming you may find many examples
 over the Internet but you always need to tweak them a bit to see
 how you can get to an example which answers your needs precisely.
@@ -14,8 +14,8 @@ add your example as a new example or contribute it back to this project.
 This is a much better way of programming since you are not doing
 the experiments on your full system but rather on a small example
 that is easy to run, debug and manage. Once you have the example ready and
-running correctly you can incorporate its code into a larger project."""
-keywords = [
+running correctly you can incorporate its code into a larger project.]]
+KEYWORDS = {
     "demos",
     "python",
-]
+}
